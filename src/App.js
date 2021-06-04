1d6c94f5efab0ae7d81f5600ca391c22be239141
kaliandkanchan <kaliandkanchan@kaliandkanchans-MacBook-Pro.local>
2021-06-04 01:16:55 -0400

    updating shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 //routing code
 //switch component
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 // the exact keyword here routes the app to the exact page component
 // when you go this link here /hats it will show HatsPage only if theres an exact keyword
@@ -21,7 +16,7 @@ function App() {
     <div>
     <Switch>
      <Route exact path='/' component={ HomePage } />
-     <Route path='/hats' component={ HatsPage } />
+     <Route path='/shop' component={ ShopPage } />
      </Switch>
     </div>
   )
